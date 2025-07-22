9b85ad17362e2ebe70544dab48a794a5e747b347
kajalB <bhangarekajal04@gmail.com>
2025-07-22 16:29:36 +0530

    Change Heading

---
+++ src/component/ChangeHeading.jsx
@@ -0,0 +1,17 @@
+import React,{useState}from 'react'
+
+const ChangeHeading = () => {
+    const [heading, setHeading] = useState('Learned HTML CSS Javascript')
+    function handleChangeHeading(){
+        setHeading('Now learing ReactJS')
+
+    }
+  return (
+    <div>
+      <h1>{heading}</h1>
+      <button onClick={handleChangeHeading}> Change Heading</button>
+    </div>
+  )
+}
+
+export default ChangeHeading
